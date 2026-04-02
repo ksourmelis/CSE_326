3c6a4705050d2ea0e7570e6b07e1c56ce2f09767
Kyriakos Sourmelis <kt.sourmelis@edu.cut.ac.cy>
2026-04-02 14:04:34 +0300

    Initial authentication setup feature

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>POTHEN ESXES</title>
-</head>
-<body>
-    <h1>Welcome to MAIN PAGE</h1>
-    <nav>
-        <ul>
-            <li><a href="/admin/admin.php">Admin</a></li>
-            <li><a href="/search/search.php">Search</a></li>
-            <li><a href="/submit/submit.php">Submit</a></li>
-        </ul>
-    </nav>
-</body>
-</html>
+<?php
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    header('Location: /modules/dashboard.php');
+    exit;
+}
+
+header('Location: /auth/login.php');
+exit;
